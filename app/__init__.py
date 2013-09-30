696e4ca706a75ed3f11fa01f2161a1cfb40c8c7a
Surendran Mahendran <surenspost@gmail.com>
2013-09-30 19:04:49 +0530

    Render index template

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
 
+app = Flask(__name__, static_folder="./static")
 
 @app.route("/")
 def hello():
-  return "hello world!! from CGI bin"
+  return render_template('index.html')
 
 if __name__ == "__main__":
   app.run()
