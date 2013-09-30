90b1ff2bf64065ed2097a9ee7bdbf656a890754a
Surendran Mahendran <surenspost@gmail.com>
2013-09-30 21:03:14 +0530

    Status data first cuts using sh

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,28 @@ from flask import Flask
 from flask import render_template
 
 import os
+import sh
+import xmltodict
+
 from config import *
 
 app = Flask(__name__, static_folder="./static")
 
 if os.getenv('DEPLOYMENT') == 'development':
-	app.config.from_object('app.config.DevelopmentConfig')
+    app.config.from_object('app.config.DevelopmentConfig')
 else:
-	app.config.from_object('app.config.ProductionConfig')
-	
+    app.config.from_object('app.config.ProductionConfig')
+
 @app.route("/")
-def hello():
-  return render_template('index.html')
+def index():
+    return render_template('index.html')
+
+@app.route("/status-data")
+def status_data():
+    qstat = sh.Command("qstat")
+    raw_jobs_xml = qstat("-u", "*", "-r", "-xml")
+    return raw_jobs_xml
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
