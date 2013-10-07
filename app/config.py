3452e10ab293e960da262c5b4f646969e30e0455
Surendran Mahendran <surenspost@gmail.com>
2013-10-07 23:18:59 +0530

    Adding a qstat command

--- app/config.py
+++ app/config.py
@@ -1,8 +1,10 @@
 class Config(object):
    	BASE_URL = ''
+   	QSTAT_COMMAND = ''
 
 class ProductionConfig(Config):
 	BASE_URL = "http://tools.wmflabs.org/tools-dashboard/"
+	QSTAT_COMMAND = "/usr/bin/qstat -u '*' -r -xml"
 
 class DevelopmentConfig(Config):
-	pass
+	QSTAT_COMMAND = "cat ./sample/qstat_output.xml"
