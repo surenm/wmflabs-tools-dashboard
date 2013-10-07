3cde58a19f49eb7b12ad83a6b9a01350ff48e364
Surendran Mahendran <surenspost@gmail.com>
2013-10-08 00:47:03 +0530

    Add error log

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,11 @@
 import site
 site.addsitedir("/data/project/tools-dashboard/python/lib/python2.7/site-packages")
 
+import sys
+sys.stderr = open('./logs/error.log', 'w')
+
 from wsgiref.handlers import CGIHandler
 from app import app
 
 CGIHandler().run(app)
+
